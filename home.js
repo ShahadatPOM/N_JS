57489a901c673d467cdb47f04efd9f82b976a0c0
ShahadatPOM <naiem.3112@gmail.com>
2020-03-11 03:39:58 +0600

    console.log, alert, getElementById, prompt

---
+++ home.js
@@ -0,0 +1,12 @@
+console.log('began 11/3/20');
+
+// variable
+var a = 'something';
+console.log(a);
+
+var num = 43;
+console.log(num);
+
+var ques = prompt('What is your goal');
+
+document.getElementById('para').innerHTML = "You Have Any Question "+ques +"?";
