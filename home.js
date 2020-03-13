027ac1c1d045756766d129ab95af24b5c1b1d6da
ShahadatPOM <naiem.3112@gmail.com>
2020-03-13 15:50:02 +0600

    Data types and string in JS

--- home.js
+++ home.js
@@ -43,8 +43,8 @@ sum(4, 6);*/
 // while vs for
 
 // while loops
+/*
 var num = 0;
-
 while(num < 100){
 	 num += 30;
 	console.log(num);
@@ -53,4 +53,29 @@ while(num < 100){
 // for loop
 for(var i=1; i<=10; i++){
 	console.log(i);
-}
+}*/
+// Data types
+let youAge =23;//number
+let yourName = 'Naiem';//string
+let fullName = {first: 'Md. Shahadat Hossain', last: 'Naiem'}; //object
+let truth = false; //boolean
+let groceries = ['apple', 'dab', 'banana', 'oranges'];//array
+let random; //undefined
+let nothing = null;//value null
+
+// String in JS
+let fruit = 'banana, blackberry';
+let moreFruit = 'banana\napple'; //new line
+console.log(fruit);
+console.log(moreFruit);
+console.log(fruit.length);//find the length of a string
+console.log(moreFruit.length);//find the length of a string
+console.log(fruit.indexOf('a'));//find the position of an alphabet within a string
+console.log(moreFruit.indexOf('l'));//find the position of an alphabet within a string
+console.log(fruit.slice(1, 5));//slice the string by given start and end index number(substring not include end index no alphabet)
+console.log(fruit.replace('nana', '3112'));//replacing a string value
+console.log(fruit.charAt(0));
+console.log(fruit[2]);
+console.log(fruit.toUpperCase());
+console.log(fruit.toLowerCase());//
+console.log(fruit.split(','));//split by a given separator
