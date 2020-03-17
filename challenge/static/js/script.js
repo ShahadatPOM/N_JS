51f15a02e7d7e2032c41bf88b5fd783663405f8e
ShahadatPOM <naiem.3112@gmail.com>
2020-03-18 00:14:52 +0600

    challenge-1 done (day or years count from given age)

---
+++ challenge/static/js/script.js
@@ -0,0 +1,19 @@
+console.log("17 mar 2020");
+
+function ageTaker() {
+	var birthYear = prompt('What year you were born?');
+	var ageInYears = (2019 - birthYear);
+	var ageInDays = (2019 - birthYear) * 365;
+
+	var h1 = document.createElement('h1');
+	var text = document.createTextNode('You are ' + ageInDays + " days or " + ageInYears + " years old");
+	h1.setAttribute('id', 'text');
+	h1.appendChild(text);
+	document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset() {
+	document.getElementById('text').remove();
+}
+
+
